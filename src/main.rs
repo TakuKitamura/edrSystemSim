669e1b57d350f806eb52c79688c9e6968fd2d29c
TakuKitamura <takukitamura.io@gmail.com>
2021-07-20 11:59:28 +0900

    can packet logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use socketcan::*;
+
 fn main() {
-    println!("Hello, world!");
+    // open can device
+    let vcan = CANSocket::open("vcan0");
+
+    // match vcan
+    match vcan {
+        Ok(can) => {
+            // infinite loop
+            loop {
+                // read can frame
+                let can_frame = can.read_frame();
+                // print can frame
+                println!("{:?}", can_frame);
+
+                // match can_frame
+                match can_frame {
+                    Ok(frame) => {
+                    },
+                    Err(err) => {
+                        // print error
+                        println!("{}", err);
+                    }
+                }
+            }  
+        }
+        Err(e) => {
+            // print canSocketOpenError message
+            println!("{}", e);
+            return;
+        }
+    }
 }
