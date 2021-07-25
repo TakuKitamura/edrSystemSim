39d4991518a84be3b2e3959aa22b87ec6c1b1bc9
TakuKitamura <takukitamura.io@gmail.com>
2021-07-26 08:14:43 +0900

    add edrsystem template

--- src/socketcan.c
+++ src/socketcan.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <net/if.h>
 #include <sys/types.h>
@@ -26,14 +27,14 @@ int main(void)
     {
         struct ifreq ifr;
         const char *ifname = "vcan0";
-        (void)strcpy(ifr.ifr_name, ifname);
+        strcpy(ifr.ifr_name, ifname);
         ioctl(s, SIOCGIFINDEX, &ifr);
 
         struct sockaddr_can addr;
         addr.can_family = AF_CAN;
         addr.can_ifindex = ifr.ifr_ifindex;
 
-        (void)printf("%s at index %d\n", ifname, ifr.ifr_ifindex);
+        // printf("%s at index %d\n", ifname, ifr.ifr_ifindex);
 
         if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1)
         {
@@ -53,29 +54,45 @@ int main(void)
                 // CANパケットキャプチャ
                 int n_bytes = read(s, &frame, sizeof(struct can_frame));
 
-                (void)printf("Read %d bytes\n", n_bytes);
-
-                struct struct_ret checkFrameData =
-                    checkFrame(frame.can_id, frame.can_dlc, frame.data); // F*言語関数
-
-                if ((checkFrameData.value == 1) && (checkFrameData.struct_error.code > 0))
+                // printf("Read %d bytes\n", n_bytes);
+                printf("can_id = 0x%x, can_dlc %d, can_data = [", frame.can_id, frame.can_dlc);
+                for (int i = 0; i < 8; i++)
                 {
+                    printf("0x%x", frame.data[i]);
+                    if (i == 7)
+                    {
+                        printf("]\n");
+                    }
+                    else
+                    {
+                        printf(", ");
+                    }
+                }
+
+                // F* Func
+                struct struct_ret checkFrameData =
+                    checkFrame(frame.can_id, frame.can_dlc, frame.data);
+
+                // valid frame
+                if ((checkFrameData.value == 1) && (checkFrameData.struct_error.code == 0))
+                {
+                    // Rust Func
                     struct struct_ret edr = recordInEdr(frame.can_id, frame.can_dlc, frame.data);
 
-                    if (edr.struct_error.code > 0)
+                    if (edr.struct_error.code > 0 || edr.value != = 1)
                     {
-                        (void)printf("record edr error message: %s(%d)", edr.struct_error.message, edr.struct_error.code);
+                        printf("record edr error message: %s(%d)", edr.struct_error.message, edr.struct_error.code);
                         continue;
                     }
                     writeToEdrFlag = edr.value
                 }
                 else
                 {
-                    (void)printf("parse frame error message: %s(%d)", checkFrameData.struct_error.message, checkFrameData.struct_error.code)
+                    printf("parse frame error message: %s(%d)", checkFrameData.struct_error.message, checkFrameData.struct_error.code)
                 }
             }
         }
     }
 
     return return_code;
-}
+}:
